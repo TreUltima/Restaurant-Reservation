409223b05126ae8f40b573def3d18eecf25bc87a
Trevon Eggleston <117423718+TreUltima@users.noreply.github.com>
2023-04-14 14:54:27 -0400

    Update reservations.service.js

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -10,39 +10,37 @@ function list(reservation_date) {
 
 function search(mobile_number) {
   return knex("reservations")
-    .whereRaw(
-      "translate(mobile_number, '() -', '') like ?",
-      `%${mobile_number.replace(/\D/g, "")}%`
-    )
-    .orderBy("reservation_date");
+      .select("*")
+      .where("mobile_number", "like", `${mobile_number}%`)
+      .orderBy("reservation_date");
 }
 
 function read(reservation_id) {
   return knex("reservations").select("*").where({ reservation_id }).first();
 }
 
-function create(reservation) {
-  return knex("reservations")
+async function create(reservation) {
+  const createdRecords = await knex("reservations")
     .insert(reservation)
-    .returning("*")
-    .then((createdRecords) => createdRecords[0]);
+    .returning("*");
+  return createdRecords[0];
 }
 
-function update(updatedRes) {
+async function update(updatedRes) {
   console.log("updatedRes", updatedRes);
-  return knex("reservations")
+  const createdRecords = await knex("reservations")
     .select("*")
     .where({ reservation_id: updatedRes.reservation_id })
-    .update(updatedRes, "*")
-    .then((createdRecords) => createdRecords[0]);
+    .update(updatedRes, "*");
+  return createdRecords[0];
 }
 
-function updateStatus(reservation_id, status) {
-  return knex("reservations")
+async function updateStatus(reservation_id, status) {
+  const createdRecords = await knex("reservations")
     .select("*")
     .where({ reservation_id })
-    .update({ status: status }, "*")
-    .then((createdRecords) => createdRecords[0]);
+    .update({ status: status }, "*");
+  return createdRecords[0];
 }
 
 module.exports = {
